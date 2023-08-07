d824aa54d3b236cb5988525caf156657e42beb56
penny <penny11124@gmail.com>
2023-08-08 02:18:54 +0800

    Menu

---
+++ Menu/app/src/main/java/com/techmania/menu/MainActivity.java
@@ -0,0 +1,29 @@
+package com.techmania.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Java
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu,menu);
+        return true;
+    }
+//  //Kotlin
+//  override fun onCreateOptionsMenu(menu: Menu):Boolean{
+//      menuInflater.inflate(R.menu.my_menu,menu)
+//      return true
+//  }
+}
