7fdb4913c70d133565ba3376e1e2065a306470c5
penny <penny11124@gmail.com>
2023-08-09 02:10:42 +0800

    French Teacher App

---
+++ FrenchTeacherApp/app/src/main/java/com/techmania/frenchteacherapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.techmania.frenchteacherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SayTheColor(View view) {
+        Button button = (Button) view;
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,
+                getResources().getIdentifier(
+                    button.getTag().toString(), "raw",getPackageName()
+                )
+        );
+        mediaPlayer.start();
+    }
+}
